da5f772dc5b760c9cc64ca36cdfbbd9ad052fceb
Chris Sun <sunhang0823@gmail.com>
2019-05-09 17:00:39 -0400

    a

--- WebContent/WEB-INF/HelloPage.jsp
+++ WebContent/WEB-INF/HelloPage.jsp
@@ -4,7 +4,7 @@
 <html>
 <body>
   <h1>First Spring MVC Application Demo</h1>
-  <h2>${welcomeMessage}</h2>
+  <h2>${msg}</h2>
 
 </body>
 </html>
--- src/com/gontuseries/hellocontroller/HelloController.java
+++ src/com/gontuseries/hellocontroller/HelloController.java
@@ -1,17 +1,21 @@
 package com.gontuseries.hellocontroller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+
 public class HelloController {
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
+	
+	@RequestMapping("/welcome/countryName/{userName}")
+	public ModelAndView helloWorld(@PathVariable("userName") String name) {
 		ModelAndView model = new ModelAndView("HelloPage");
-		model.addObject("welcomeMessage","hello world");
+		model.addObject("msg","hello " + name);
+		
 		return model;
 	}
-
-
+	
+	
 }
